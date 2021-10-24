0fc511884b3d68480f97d3a7c62a83efbfec4209
Miguel <mmorea@gmail.com>
2021-10-24 09:26:30 +0200

    Subida 6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { IfcAPI } from "web-ifc/web-ifc-api";
-console.log("IfcAPI");
-console.log("ya estamos watcheando, no? o qué)")
+console.log( IfcAPI );
+console.log("ya estamos watcheando, no? o quéddfasdfadf)");
 console.log("no, aún no");

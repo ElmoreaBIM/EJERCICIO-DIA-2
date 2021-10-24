5dbfe85606e90b5511c15e1fc35cb5a9ed620c5d
Miguel <mmorea@gmail.com>
2021-10-24 18:43:30 +0200

    Subida 15

--- bundle.js
+++ bundle.js
@@ -36360,3 +36360,12 @@ console.log("ya estamos watcheando, no?)");
 console.log("que sí, coño!");
 console.log("No digas tacos");
 console.log("Salvo que sea necesario, piltrafillasssss");
+const button=document.getElementById("Boton1");
+const SelectorFichero=document.getElementById("input-element");
+ button.onclick=()=>{
+     SelectorFichero.click();
+ };
+ SelectorFichero.onchange=(changed)=>{
+     console.log(changed);
+ };
+console.log(button);
